a2ef4a1d65faf18be03e0768405a49db56efd326
Hammouche-Med <mhammouche995@gmail.com>
2023-03-06 00:14:26 +0100

    up

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import './assets/main.css'
+import "popper.js/dist/popper.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,57 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+import navBar from "../components/navBar.vue";
+
+const title = ref("");
+const slug = ref("");
+
+const formatString = (str) => {
+  slug.value = str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <navBar />
+  <div class="container">
+    <section>
+      <h2 class="mb-4">Title2Slug</h2>
+
+      <p>format a title to slug for blogs articles</p>
+      <section class="pb-4">
+        <div class="bg-white border rounded-5">
+          <section
+            class="w-100 p-4 pb-4 d-flex justify-content-center align-items-center flex-column"
+          >
+            <div>
+              <div class="input-group">
+                <div class="form-outline">
+                  <input
+                    placeholder="enter your title"
+                    id="form1"
+                    class="form-control"
+                    v-model="title"
+                    @change="formatString(title)"
+                  />
+                  <label
+                    class="form-label"
+                    for="form1"
+                    style="margin-left: 0px"
+                  ></label>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <div class="p-4 text-center border-top mobile-hidden">
+            {{ slug }}
+          </div>
+        </div>
+      </section>
+    </section>
+  </div>
 </template>
